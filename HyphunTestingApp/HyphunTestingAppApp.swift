e6b091cf9f5c6e911d074c98447ba77369354601
Roshan <141453679+roshan-tlc@users.noreply.github.com>
2025-09-21 21:22:13 +0530

    Initial Commit

---
+++ HyphunTestingApp/HyphunTestingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyphunTestingAppApp.swift
+//  HyphunTestingApp
+//
+//  Created by Krithik Roshan on 21/09/25.
+//
+
+import SwiftUI
+
+@main
+struct HyphunTestingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HyphunTestingAppTests/HyphunTestingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HyphunTestingAppTests.swift
+//  HyphunTestingAppTests
+//
+//  Created by Krithik Roshan on 21/09/25.
+//
+
+import Testing
+@testable import HyphunTestingApp
+
+struct HyphunTestingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
